150adf4f4c0085f7ca4e2a60b1eaa91af9ebee91
Demon <am8680623@gmail.com>
2023-12-01 22:41:44 +0630

    Auto installer

--- Burp Auto installer.sh
+++ Burp Auto installer.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+#install gnome-terminal
 if [[ -e /bin/gnome-terminal || -e /usr/bin/gnome-terminal ]]
  then
     sudo apt install gnome-terminal
  fi
+
 if [[ -e /home/$USER/BurpSuitePro ]]
  then 
  echo "BurpSuitpro is installed"
  read -p  "Crack Now ?[Y,n] : " crak
- fi
-
 if [[ "$crak" != "n" || "$crak" != "N" ]]
  then
+exit 0;
+else
  cp keygen.jar ~/BurpSuitePro/
  cp loader.jar ~/BurpSuitePro/
  gnome-terminal -- bash -c "java -jar /home/$USER/BurpSuitePro/keygen.jar ; exec bash" &
@@ -18,20 +20,24 @@ if [[ "$crak" != "n" || "$crak" != "N" ]]
  cp /home/$USER/BurpSuitePro/BurpSuitePro /home/$USER/BurpSuitePro/BurpSuitePro_org
  echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:\"/home/$USER/BurpSuitePro/loader.jar\" -noverify -jar \"/home/$USER/BurpSuitePro/burpsuite_pro.jar\"" >> /home/$USER/BurpSuitePro/BurpSuitePro ; 
 fi
+fi
+
+
 if [[ ! -e /home/$USER/BurpSuitePro ]]
 then
     echo "BurpSuitpro is not installed "
     echo "Please install at the /home/$USER/"
     read -p "Install now ? [Y,n]:" ins 
-fi
 if [[ $ins == "N" || $ins ==  "n" ]]
 then
     exit 0;
 else
     clear
-    echo "Please install at the /home/$USER/"
+    echo "Please install at the /home/$USER/" 
     wget https://portswigger-cdn.net/burp/releases/download?product=pro\&version=2022.8.5\&type=Linux
     bash 'download?product=pro&version=2022.8.5&type=Linux'
     echo "Rerun Script"
+fi
+
 fi
 
